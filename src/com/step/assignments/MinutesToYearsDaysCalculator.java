f8f8a430b862077be835323c925c99802385a6ca
moumita das <44020214+moumitaguri@users.noreply.github.com>
2019-03-06 15:37:41 +0530

    Solved MinutesToYearsDaysCalculator problem.

---
+++ src/com/step/assignments/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,21 @@
+package com.step.assignments;
+
+public class MinutesToYearsDaysCalculator {
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0){
+            System.out.println("Invalid Value");
+            return;
+        }
+        long days = minutes/(60*24);
+        long years = days/365;
+        days = days % 365;
+        System.out.println(minutes + " min = " + years + " y and " + days + " d");
+    }
+}
